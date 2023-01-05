6b8b6272fbe2d496c2d1ec0913328b374281ffad
Jeremy Watt <jermwatt@gmail.com>
2023-01-05 16:24:12 -0700

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('jermwatt')
+            node / gitConfigEmail('jermwatt@gmail.com')
         }
     }
     triggers {
